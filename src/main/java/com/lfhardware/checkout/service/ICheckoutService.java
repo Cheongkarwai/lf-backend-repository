db3ba27a75d34a311b5ecc5fa419f90c52e472e2
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-02 00:18:30 +0800

    create a findById (session) method

--- src/main/java/com/lfhardware/checkout/service/ICheckoutService.java
+++ src/main/java/com/lfhardware/checkout/service/ICheckoutService.java
@@ -1,11 +1,19 @@
 package com.lfhardware.checkout.service;
 
 import com.lfhardware.checkout.dto.CheckoutInput;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.model.checkout.Session;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
 public interface ICheckoutService {
 
-    Mono<String> createCheckoutSession(CheckoutInput checkoutInput);
+//    String createCheckoutSession(CheckoutInput checkoutInput) throws StripeException;
+    Mono<Session> createCheckoutSession(String transferGroup, CheckoutInput checkoutInput);
+
+    Mono<Session> findById(String id);
+
+
 }
