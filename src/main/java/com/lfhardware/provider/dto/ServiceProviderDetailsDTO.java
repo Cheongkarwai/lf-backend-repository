406b8ccfd81cd3bbf5e18e3da56e0bb252fb6c23
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-02 00:00:35 +0800

    transform record into DTO class

--- src/main/java/com/lfhardware/provider/dto/ServiceProviderDetailsDTO.java
+++ src/main/java/com/lfhardware/provider/dto/ServiceProviderDetailsDTO.java
@@ -5,24 +5,67 @@ import com.lfhardware.auth.dto.AddressDTO;
 import com.lfhardware.provider.domain.Status;
 import com.lfhardware.provider_business.dto.ServiceDTO;
 import com.lfhardware.shared.BaseAddress;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
-public record ServiceProviderDetailsDTO(String id, String name,
-                                        @JsonProperty("contact_info") ContactInfoDTO contactInfo,
-                                        @JsonProperty("is_verified") boolean isVerified, @JsonProperty double rating,
-                                        List<ServiceDTO> services, String overview,
-                                        @JsonProperty("state_coverages") Set<String> stateCoverages,
-                                        @JsonProperty("country_coverages") Set<String> countryCoverages,
-                                        @JsonProperty("city_coverages") Set<String> cityCoverages, Status status,
-                                        @JsonProperty("created_at") LocalDateTime createdAt,
-                                        @JsonProperty("business_profile_image") String businessProfileImage,
-                                        Set<ServiceProviderReviewDTO> reviews,
-                                        @JsonProperty("social_media") SocialMediaLinkDTO socialMedia,
-                                        @JsonProperty("stripe_account_id") String stripeAccountId,
-                                        @JsonProperty("front_identity_card") String frontIdentityCard,
-                                        @JsonProperty("back_identity_card") String backIdentityCard,
-                                        @JsonProperty AddressDTO address) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceProviderDetailsDTO {
+
+    private String id;
+
+    private String name;
+
+    @JsonProperty("contact_info")
+    private ContactInfoDTO contactInfo;
+
+    @JsonProperty("is_verified")
+    private boolean isVerified;
+
+    private double rating;
+
+    private List<ServiceDTO> services;
+
+    private String overview;
+
+    @JsonProperty("state_coverages")
+    private Set<String> stateCoverages;
+
+    @JsonProperty("country_coverages")
+    private Set<String> countryCoverages;
+
+    @JsonProperty("city_coverages")
+    private Set<String> cityCoverages;
+
+    private Status status;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("business_profile_image")
+    private String businessProfileImage;
+
+    private Set<ServiceProviderReviewDTO> reviews;
+
+    @JsonProperty("social_media")
+    private SocialMediaLinkDTO socialMedia;
+
+    @JsonProperty("stripe_account_id")
+    private String stripeAccountId;
+
+    @JsonProperty("front_identity_card")
+    private String frontIdentityCard;
+
+    @JsonProperty("back_identity_card")
+    private String backIdentityCard;
+
+    private AddressDTO address;
 }
