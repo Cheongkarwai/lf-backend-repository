42a2b1cbf3da7c0a14e2a12dc2b5d0e530b43f29
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-02 00:00:28 +0800

    transform record into DTO class

--- src/main/java/com/lfhardware/provider/dto/ServiceProviderDTO.java
+++ src/main/java/com/lfhardware/provider/dto/ServiceProviderDTO.java
@@ -2,19 +2,48 @@ package com.lfhardware.provider.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lfhardware.provider.domain.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
-public record ServiceProviderDTO(String id,
-                                 String name,
-                                 @JsonProperty("contact_info") ContactInfoDTO contactInfo,
-                                 @JsonProperty("is_verified") boolean isVerified,
-                                 @JsonProperty double rating,
-                                 @JsonProperty("stripe_account_id") String stripeAccountId,
-                                 @JsonProperty("front_identity_card") String frontIdentityCard,
-                                 @JsonProperty("back_identity_card") String backIdentityCard,
-                                 @JsonProperty("ssm") String ssm,
-                                 Status status,
-                                 @JsonProperty("business_profile_image") String businessProfileImage
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceProviderDTO {
+
+    private String id;
+
+    private String name;
+
+    @JsonProperty("contact_info")
+    private ContactInfoDTO contactInfo;
+
+    @JsonProperty("is_verified")
+    private boolean isVerified;
+
+    private double rating;
+
+    @JsonProperty("social_media")
+    private SocialMediaLinkDTO socialMedia;
+
+    @JsonProperty("stripe_account_id")
+    private String stripeAccountId;
+
+    @JsonProperty("front_identity_card")
+    private String frontIdentityCard;
+
+    @JsonProperty("back_identity_card")
+    private String backIdentityCard;
+
+    @JsonProperty("ssm")
+    private String ssm;
+
+    private Status status;
+
+    @JsonProperty("business_profile_image")
+    private String businessProfileImage;
 }
