957576dd13528f744c46b34e0e34bf8b21358563
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-07-01 23:33:01 +0800

    use dropdown for city, state control

--- src/main/java/com/lfhardware/auth/domain/BasicInformation.java
+++ src/main/java/com/lfhardware/auth/domain/BasicInformation.java
@@ -2,8 +2,10 @@ package com.lfhardware.auth.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lfhardware.provider.domain.ServiceDetails;
+import com.lfhardware.provider.domain.SocialMediaLink;
 import com.lfhardware.provider.dto.BusinessDetailsInput;
 import com.lfhardware.provider.dto.ServiceDetailsInput;
+import com.lfhardware.provider.dto.SocialMediaLinkInput;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +15,9 @@ public class BasicInformation {
 
     @JsonProperty("business_details")
     private BusinessDetailsInput businessDetails;
+
+    @JsonProperty("social_media")
+    private SocialMediaLinkInput socialMediaLink;
 
     @JsonProperty("service_details")
     private ServiceDetailsInput serviceDetails;
