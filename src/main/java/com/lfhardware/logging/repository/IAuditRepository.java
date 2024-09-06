3775d84b54b8e930892e832cd35fde7b2e95eec2
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-09-06 16:13:41 +0800

    delete unused code

--- src/main/java/com/lfhardware/logging/repository/IAuditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lfhardware.logging.repository;
-
-import com.lfhardware.logging.domain.Audit;
-import com.lfhardware.shared.CrudRepository;
-
-public interface IAuditRepository extends CrudRepository<Audit, Long> {
-}
